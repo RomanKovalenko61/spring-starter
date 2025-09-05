29de20fdd38143079db3f00b91a162cad822b3f0
Roman <astek14@mail.ru>
2025-09-05 19:29:07 +0300

    @Scope

--- src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -5,12 +5,15 @@ import com.dmdev.spring.bpp.Transaction;
 import com.dmdev.spring.database.entity.Company;
 import com.dmdev.spring.database.pool.ConnectionPool;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
 import java.util.Optional;
 
-
+@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
