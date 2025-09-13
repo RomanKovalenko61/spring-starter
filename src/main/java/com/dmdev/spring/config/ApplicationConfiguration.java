98e985bf6bb89c54b686fb66038acc344e7af04d
Roman <astek14@mail.ru>
2025-09-13 23:49:52 +0300

    @Bean part 1

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,12 +1,17 @@
 package com.dmdev.spring.config;
 
+import com.dmdev.spring.database.pool.ConnectionPool;
 import com.dmdev.spring.database.repository.CrudRepository;
+import com.dmdev.spring.database.repository.UserRepository;
 import com.dmdev.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.stereotype.Component;
 
-@ImportResource("classpath:application.xml")
+//@ImportResource("classpath:application.xml")
 @Import({WebConfiguration.class})
 @Configuration
 @PropertySource("classpath:application.properties")
@@ -18,4 +23,16 @@ import org.springframework.stereotype.Component;
                 @Filter(type = FilterType.REGEX, pattern = "com\\..+Repository"),
         })
 public class ApplicationConfiguration {
+
+    @Bean("pool2")
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ConnectionPool pool2(@Value("${db.username}") String username) {
+        return new ConnectionPool(username, 20);
+    }
+
+    @Bean
+    // @Qualifier("pool2") or parameter name pool2
+    public UserRepository userRepository2(@Qualifier("pool2") ConnectionPool pool2) {
+        return new UserRepository(pool2);
+    }
 }
