4d4bddc815f54e63ba5e38f6901fea4991cee815
Roman <astek14@mail.ru>
2025-09-13 23:29:08 +0300

    @Import and @ImportResource

--- src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/dmdev/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,13 @@
 package com.dmdev.spring.config;
 
 import com.dmdev.spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
+import com.dmdev.web.config.WebConfiguration;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@ImportResource("classpath:application.xml")
+@Import({WebConfiguration.class})
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.dmdev.spring",
